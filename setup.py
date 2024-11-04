7e0af7df80f926a653f8f8a48d52520735c8a493
Hunia Fatima <hunia.fatima@arbisoft.com>
2024-11-04 12:43:29 +0500

    chore: update python version from 3.11 to 3.12

--- setup.py
+++ setup.py
@@ -113,7 +113,7 @@ setup(
     ],
     include_package_data=True,
     install_requires=load_requirements('requirements/base.in'),
-    python_requires=">=3.8",
+    python_requires=">3.8",
     license="AGPL 3.0",
     zip_safe=False,
     keywords='Python edx',
@@ -123,6 +123,6 @@ setup(
         'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.12',
     ],
 )
